cef466315318f4f15f92ce57a812d92bb1b53fee
kaleb09 <blekaadamu979@gmail.com>
2024-10-02 10:27:41 +0300

    test commit

--- app/src/main/java/net/geidea/payment/DBHandler.kt
+++ app/src/main/java/net/geidea/payment/DBHandler.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import net.geidea.payment.transaction.model.TransData
-// test commit
+// kaleb test commit
 class DBHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
     companion object {
         private const val DATABASE_NAME = "UsersDatabase.db"
